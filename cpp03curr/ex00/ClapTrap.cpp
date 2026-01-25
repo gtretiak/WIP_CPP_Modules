a636f3186f452c5c3a1ec38d78e280d0420625d2
gtretiak <george22021988@gmail.com>
2026-01-25 21:27:50 +0000

    ex00 done

--- cpp03curr/ex00/ClapTrap.cpp
+++ cpp03curr/ex00/ClapTrap.cpp
@@ -2,11 +2,9 @@
 #include <string>
 #include <iostream>
 
-ClapTrap::ClapTrap(std::string	name) : name_(name) {
-	this->health_ = 10;
-	this->energy_ = 10;
-	this->damage_ = 0;
-	std::cout << "Robot named " << this->name_ << " constructed" << std::endl;
+ClapTrap::ClapTrap(std::string	name) : name_(name), health_(10), energy_(10), damage_(0) {
+	std::cout << "Robot named " << this->name_ << " constructed.";
+	std::cout << " HP:" << health_ << ", EP:" << energy_ << ", DP:" << damage_ << std::endl;
 }
 
 ClapTrap::ClapTrap(const ClapTrap &Another) {
@@ -18,29 +16,52 @@ ClapTrap::ClapTrap(const ClapTrap &Another) {
 }
 
 ClapTrap	&ClapTrap::operator=(const ClapTrap &Another) {
-	std::cout << "Robot " << this->name_ << " reassigned. ";
+	std::cout << "Robot " << this->name_ << " is re-sleevng into ";
 	this->name_ = Another.name_;
 	this->health_ = Another.health_;
 	this->energy_ = Another.energy_;
 	this->damage_ = Another.damage_;
-	std::cout << "New name is " << this->name_ << std::endl;
+	std::cout << this->name_ << std::endl;
 	return (*this);
 }
 
 void	ClapTrap::attack(const std::string &target) {
-	std::cout << "Robot " << this->name_ << " attacks " << target.name_ << ", causing " << this->damage_ << " points of damage!" << std::endl;
+	std::cout << "Robot " << this->name_;
+	if (!this->energy_)
+	{
+		std::cout << " has no energy to attack (passed)." << std::endl;
+		return ;
+	}
+	else if (!this->health_)
+	{
+		std::cout << " has no HP to continue the fight." << std::endl;
+		return ;
+	}
+	std::cout << " draws aggro (damage increased) and attacks " << target << ", causing " << this->damage_ + 10 << " points of damage (mitigated though).";
+	this->energy_--;
+	std::cout << " EP:" << this->energy_ << std::endl;
 }
 
 void	ClapTrap::takeDamage(unsigned int amount) {
-	std::cout << "Robot " << this->name_ << " take " << amount << " points of damage." << std::endl;
+	std::cout << "Robot " << this->name_ << " takes " << amount << " points of damage.";
 	this->health_ -= amount;
+	std::cout << " HP:" << this->health_ << std::endl;
 }
 
 void	ClapTrap::beRepaired(unsigned int amount) {
-	std::cout << "Robot " << this->name_ << " got repaired by " << amount << " points." << std::endl;
+	std::cout << "Robot " << this->name_;
+      	if (!this->energy_ || !this->health_)
+	{
+		std::cout << " tries to repair itself, but fails." << std::endl;
+		return ;
+	}
+	std::cout << " repairs itself by " << amount << " points."; 
 	this->health_ += amount;
+	this->energy_--;
+	std::cout << " HP:" << this->health_;
+	std::cout << ". EP:" << this->energy_ << std::endl;
 }
 
 ClapTrap::~ClapTrap() {
-	std::cout << "Robot named " << this->name_ << " desctructed" << std::endl;
+	std::cout << "Robot named " << this->name_ << " destroyed" << std::endl;
 }
--- cpp03curr/ex00/main.cpp
+++ cpp03curr/ex00/main.cpp
@@ -1,5 +1,63 @@
 #include "ClapTrap.hpp"
+#include <string>
+#include <iostream>
 
 int	main(void) {
+	std::string	robot[4] = {"Optimus Prime", "R2-D2", "T1000", "Terminator"};
+	ClapTrap	Op(robot[0]);
+	ClapTrap	R2(robot[1]);
+	ClapTrap	T1000orig(robot[2]);
+	ClapTrap	Terminator(robot[3]);
+	std::cout << "\nRound 1.\n" << std::endl;
+	Op.attack(robot[1]); //Op=9E
+	R2.takeDamage(4); //R2=6HP
+	R2.attack(robot[0]); 
+	Op.takeDamage(2); //Op=8HP
+	std::cout << "\nRound 2.\n" << std::endl;
+	Op.attack(robot[1]); //Op=8E
+	R2.takeDamage(4); //R2=2HP
+	R2.beRepaired(2); //R2=4HP
+	std::cout << "\nRound 3.\n" << std::endl;
+	Op.attack(robot[1]); //Op=7E
+	R2.takeDamage(4); //R2=0HP
+	R2.attack(robot[0]); //can't attack (no health)
+	ClapTrap	T1000copy(T1000orig); //Copy constructor
+	T1000copy.attack(robot[0]);
+	Op.takeDamage(3); //Op=5HP
+	std::cout << "\nRound 4.\n" << std::endl;
+	Op.attack(robot[2]); //Op=6E
+	T1000copy.takeDamage(4); //T1000=6HP
+	T1000copy.attack(robot[0]);
+	Op.takeDamage(3); //Op=2HP
+	std::cout << "\nRound 5.\n" << std::endl;
+	Op.beRepaired(4); //Op=6HP, 5E
+	T1000copy.attack(robot[0]);
+	Op.takeDamage(3); //Op=3HP
+	std::cout << "\nRound 6.\n" << std::endl;
+	Op.beRepaired(4); //Op=7HP, 4E
+	T1000copy.attack(robot[0]);
+	Op.takeDamage(3); //Op=4HP
+	std::cout << "\nRound 7.\n" << std::endl;
+	Op.beRepaired(4); //Op=8HP, 3E
+	T1000copy.attack(robot[0]);
+	Op.takeDamage(3); //Op=5HP
+	std::cout << "\nRound 8.\n" << std::endl;
+	Op.beRepaired(4); //Op=9HP, 2E
+	T1000copy.attack(robot[0]);
+	Op.takeDamage(3); //Op=6HP
+	std::cout << "\nRound 9.\n" << std::endl;
+	Op.beRepaired(4); //Op=10HP, 1E
+	T1000copy.attack(robot[0]);
+	Op.takeDamage(3); //Op=7HP
+	std::cout << "\nRound 10.\n" << std::endl;
+	Op.attack(robot[2]); //Op=0E
+	T1000copy.takeDamage(4); //T1000=2HP
+	T1000copy.beRepaired(3); //T1000=5HP
+	std::cout << "\nRound 11.\n" << std::endl;
+	Op.attack(robot[2]); //can't attack (no energy)
+	Op = Terminator; //Copy assignment operator
+	Op.attack(robot[2]);
+	T1000copy.takeDamage(5); //T1000=0HP
+	std::cout << "Optimus Prime in the Terminator's shell win!" << std::endl;
 	return 0;
 }
