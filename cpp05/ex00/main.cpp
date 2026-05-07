e29aad8249cf038316f3ea1f1d4fc21c122ff431
Georgii Tretiakov <george22021988@gmail.com>
2026-05-07 13:26:34 +0000

    main workflow clarified

--- cpp05/ex00/main.cpp
+++ cpp05/ex00/main.cpp
@@ -3,13 +3,14 @@
 
 int	main(void) {
 	try {
-		//Bureaucrat	test1("test1", 200);
-		//Bureaucrat	test2("test2", 0);
+		//Bureaucrat	test1("test1", 200);//too low exception
+		//Bureaucrat	test2("test2", 0);//too high exception
 		Bureaucrat	n;
 		std::cout << n << std::endl;
 
 		Bureaucrat	a("Alice", 14);
 		std::cout << a << std::endl;
+		std::cout << "incrementing grade" << std::endl;
 		a.incrementGrade();
 		std::cout << a << std::endl;
 
@@ -22,14 +23,16 @@ int	main(void) {
 		Bureaucrat	c("Charlie", 77);
 		std::cout << c << std::endl;
 		c = b;
-		//c.incrementGrade();
+		//c.incrementGrade(); //too high exception
+		std::cout << "decrementing grade" << std::endl;
 		c.decrementGrade();
 		std::cout << c << std::endl;
 
 		Bureaucrat	d("Delta", 33);
 		std::cout << d << std::endl;
 		d = n;
-		//d.decrementGrade();
+		//d.decrementGrade(); //too low exception
+		std::cout << "incrementing grade" << std::endl;
 		d.incrementGrade();
 		std::cout << d << std::endl;
 	}
